6a851390ff5765c7aa1bd3b0ac5d10a024c73608
Ivan M <slade91@mail.ru>
2022-10-16 01:18:57 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Point GetPoint()
+{
+    Point point = new Point();
+    Console.WriteLine("Введите коорданату X:");
+    point.X = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y: ");
+    point.Y = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Z: ");
+    point.Z = int.Parse(Console.ReadLine());
+
+    return point;
+
+}
+
+Point firstPoint = GetPoint();
+Point secondPoint = GetPoint();
+                //Формула нахождения точек в 3D пространстве..
+double result = Math.Sqrt(Math.Pow(secondPoint.X - firstPoint.X, 2) + Math.Pow(secondPoint.Y - firstPoint.Y, 2) + Math.Pow(secondPoint.Z - firstPoint.Z, 2));
+Console.WriteLine(result);
+
+class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+}
